67aa9db089062ed80137cd25e3498ccd8b8a8a76
ruanyf <yifeng.ruan@gmail.com>
2020-05-13 11:50:26 +0800

    refactor: add support banner

--- scripts/support.js
+++ scripts/support.js
@@ -1,2 +1,47 @@
-// loaded
-console.log('loaded');
+function showSupBanner() {
+  // 2020年8月15日
+  var deadline = new Date(2020, 7, 15);
+  if (deadline - (new Date()) < 0) return;
+
+  // scope check
+  var scope = 'all';
+  var currentScope = window.location.pathname.split('/')[1];
+  if (scope !== 'all' && !scope.includes(currentScope)) return;
+
+  // page path check
+  if (typeof LOPPO === 'undefined') return;
+  if (!LOPPO.current_path || LOPPO.current_path.substr(-1) === '/') return;
+
+  // create banner node
+  var bannerDiv = document.createElement('div');
+  var bannerP = document.createElement('p');
+  bannerDiv.appendChild(bannerP);
+
+  // bannerP.innerHTML = '<picture style="display:block;text-align:center;margin-bottom:1rem;"><source srcset="https://www.wangbase.com/blogimg/asset/202005/bg2020051215.jpg" media="(min-width: 600px)"><img src="https://www.wangbase.com/blogimg/asset/202005/bg2020051216.jpg" style="max-height:200px;border-radius: 15px;text-align: center;"></picture>';
+
+  var styleStr = [
+    'margin: 2rem 0',
+    'padding: 1em',
+    'background-color: #c4e0e1',
+    'border-radius: 5px',
+    'font-size: 90%',
+    // 'font-size: 75%',
+    // 'width: 210px',
+    'color: #333333'
+  ].join(';');
+
+  var text = '【免费领取】' +
+    '廖学峰老师历时三个月整理的<span style="color: #4682BE;">《Vue3 响应式原理 & 前端面试攻略》</span>。';
+
+  bannerP.style = styleStr;
+  bannerP.innerHTML = '<a href="https://datayi.cn/w/a9BLwxNP" target="_blank" style="color: #333333;">' + text + '</a>';
+
+  // insert into page
+  var metaNode = document.querySelector('.page-meta');
+  if (!metaNode) return;
+  metaNode.parentElement.insertBefore(bannerDiv, metaNode.nextSibling);
+
+  // console.log('loaded');
+}
+
+showSupBanner();
