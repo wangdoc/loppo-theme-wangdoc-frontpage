49cd9f1fbd6eca9b96067e5d73365ec04884e6b9
ruanyf <yifeng.ruan@gmail.com>
2023-07-10 20:45:29 +0800

    chore: add mobile supporting for navbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
 var setMinHeight = require('./setMinHeight');
 var showDropdownMenu = require('./dropdown');
+var navbarHandler = require('./navbarHandler');
 
 /* set content's min-height */
 setMinHeight();
 
 /* show dropdown menu on navbar */
 showDropdownMenu();
+
+/* navabar handler */
+navbarHandler();
+
